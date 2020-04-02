7a09d314b300af71a981cce7e8062637696304b9
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-04-02 16:47:12 +0100

    Init setup

---
+++ space-race/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  space-race
+//
+//  Created by Bradley Chesworth on 02/04/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
